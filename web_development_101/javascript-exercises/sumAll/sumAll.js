9158cde0bcff85148e035c8801690120bd57aa8f
Fiali1 <fiali1@hotmail.com>
2020-01-23 19:27:50 -0300

    web_dev sumAll

--- web_development_101/javascript-exercises/sumAll/sumAll.js
+++ web_development_101/javascript-exercises/sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function(...array) {
+    let indexStart, indexEnd, type0, type1;
+    let sum = 0;
 
+    type0 = typeof(array[0]);
+    type1 = typeof(array[1]);
+
+    if(
+        type0 == "number" && 
+        type1 == "number" && 
+        array[0] >= 0 && 
+        array[1] >= 0) 
+    {
+        console.log("OK");
+        if(array[0] < array[1]) {
+            indexStart = array[0];
+            indexEnd = array[1];
+        }
+        else {
+            indexStart = array[1];
+            indexEnd = array[0];
+        }
+    }
+    else
+        return "ERROR";
+
+    for(let i = indexStart; i <= indexEnd; i++) {
+        sum += i;
+    }
+
+    return sum;
 }
-
 module.exports = sumAll

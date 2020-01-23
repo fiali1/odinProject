4dc5e6193fdb8b691bd4e5476d445a5fc9e7c6dd
Fiali1 <fiali1@hotmail.com>
2020-01-23 19:56:38 -0300

    web_dev tempConversion

--- web_development_101/javascript-exercises/tempConversion/tempConversion.js
+++ web_development_101/javascript-exercises/tempConversion/tempConversion.js
@@ -1,10 +1,28 @@
-const ftoc = function() {
+const ftoc = function(fahrenheit) {
+  let celsius = 0;
 
-}
+  celsius = fahrenheit - 32;
+  celsius *= 5;
+  celsius /= 9;
+  
+  let multiplier = Math.pow(10, 1 || 0);
+  celsius = Math.round(celsius * multiplier) / multiplier;
 
-const ctof = function() {
+  return celsius;
+};
 
-}
+const ctof = function(celsius) {
+  let fahrenheit = 0;
+
+  fahrenheit = celsius * 9;
+  fahrenheit /= 5;
+  fahrenheit += 32;
+  
+  let multiplier = Math.pow(10, 1 || 0);
+  fahrenheit = Math.round(fahrenheit * multiplier) / multiplier;
+
+  return fahrenheit;
+};
 
 module.exports = {
   ftoc,

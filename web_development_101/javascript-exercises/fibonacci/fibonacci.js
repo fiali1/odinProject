e980c047b76786d0b14477bb18be7fc88ffa37e8
Fiali1 <fiali1@hotmail.com>
2020-01-27 14:35:16 -0300

    javascript-exercises fibonacci/findOldest/getTitles

--- web_development_101/javascript-exercises/fibonacci/fibonacci.js
+++ web_development_101/javascript-exercises/fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if(isNaN(index) || index < 0) {
+        return 'OOPS';
+    }
 
+    if(index == 0)
+        return 0;
+
+    if(index == 1 ||index == 2)
+        return 1;
+
+    return fibonacci(index - 1) + fibonacci(index - 2);
 }
 
 module.exports = fibonacci
--- web_development_101/javascript-exercises/findTheOldest/findTheOldest.js
+++ web_development_101/javascript-exercises/findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+    let oldest = people.sort((a, b) => {
+        if(a.yearOfDeath == null) {
+            let currentYear = new Date().getFullYear();
 
+            let ageA = currentYear - a.yearOfBirth;
+            let ageB = b.yearOfDeath - b.yearOfBirth;
+
+            return ageA < ageB;
+        }
+
+        let ageA = a.yearOfDeath - a.yearOfBirth;
+        let ageB = b.yearOfDeath - b.yearOfBirth;
+
+        return ageA < ageB;
+    })
+
+    return oldest[0];
 }
 
 module.exports = findTheOldest

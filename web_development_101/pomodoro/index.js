54f900fcff07af1d080550ab01da46631ffe3ca5
Fiali1 <fiali1@hotmail.com>
2020-02-08 21:57:49 -0300

    pomodoro DRY2 and some fixes

--- web_development_101/pomodoro/index.js
+++ web_development_101/pomodoro/index.js
@@ -1,6 +1,6 @@
-let theme = 'sepia';
-
 function changeTheme(e, display) {
+    let theme = display.getAttribute('theme');
+
     const stylesheet = document.querySelector('#theme');
     const playPause = document.querySelector('#play-pause');
     const stop = document.querySelector('#stop');
@@ -14,26 +14,28 @@ function changeTheme(e, display) {
 
     stylesheet.setAttribute('href', `./css/styles_${theme}.css`);
 
-    if(display.getAttribute('running'))
+    if(display.getAttribute('running') != null)
         playPause.setAttribute('src', `./assets/icons/pause_${theme}.png`);
     else
         playPause.setAttribute('src', `./assets/icons/play_${theme}.png`);
     
     stop.setAttribute('src', `./assets/icons/stop_${theme}.png`);    
+
+    display.setAttribute('theme', theme);
 }
 
-function leadingZero(value, play) {
+function leadingZero(value) {
     value = value < 10 ? '0' + value : value;
     return value;
 }
 
-function reset(display) {
+function reset(display, play) {
     display.removeAttribute('running');
 
     if(display.getAttribute('initialValue') == null)
         return;
 
-    const play = document.querySelector('#play-pause');
+    const theme = display.getAttribute('theme');
     play.setAttribute('src', `./assets/icons/play_${theme}.png`);
 
     const value = display.getAttribute('initialValue');
@@ -48,11 +50,9 @@ function reset(display) {
     display.removeAttribute('initialValue');
 }
 
-function startCountdown(duration, display) {
+function startCountdown(duration, display, play) {
     if (display.getAttribute('initialValue') == null)
         display.setAttribute('initialValue', duration); 
-
-    const play = document.querySelector('#play-pause');
 
     let minutes, seconds = '';
 
@@ -63,7 +63,6 @@ function startCountdown(duration, display) {
             
             minutes = leadingZero(minutes);
             seconds = leadingZero(seconds);
-            
             
             display.textContent = minutes + ':' + seconds;
             
@@ -87,10 +86,10 @@ function parseValue(value) {
     return processed;
 }
 
-function setTimer(e, display) {
+function setTimer(e, display, play) {
     display.removeAttribute('running');
 
-    const play = document.querySelector('#play-pause');
+    const theme = display.getAttribute('theme');
     play.setAttribute('src', `./assets/icons/play_${theme}.png`);
 
     const timer = document.querySelector('#timer');
@@ -111,20 +110,11 @@ function setTimer(e, display) {
     }
 }
 
-function timestampEvents(display) {
-    const timestamps = document.querySelectorAll('.timestamp');
-
-    timestamps.forEach(timestamp => {
-        timestamp.addEventListener('click', (e) => {
-            setTimer(e, display);
-        });
-    });
-}
-
-function configCountdown(e, display) {
+function configCountdown(e, display, play) {
     if (e.target.id == 'play-pause') {
         display.toggleAttribute('running');
-
+        
+        const theme = display.getAttribute('theme');
         if (display.getAttribute('running') != null)
             e.target.setAttribute('src', `./assets/icons/pause_${theme}.png`);
         else 
@@ -137,41 +127,40 @@ function configCountdown(e, display) {
 
         duration = minutes * 60 + seconds;
 
-        startCountdown(duration, display);
+        startCountdown(duration, display, play);
     }
     else if (e.target.id == 'stop') {
-        reset(display);
+        reset(display, play);
     }
-
 }
 
-function controllerEvents(display) {
-    const controllers = document.querySelectorAll('.controller');
+function setEvents() {
+    const display = document.querySelector('#timer');
+    display.setAttribute('theme', 'sepia');
 
-    controllers.forEach(controller => {
-        controller.addEventListener('click', (e) => {
-            configCountdown(e, display);
-        });
-    });
-}
-
-function themeEvents(display) {
+    const play = document.querySelector('#play-pause');
+    
     const themes = document.querySelectorAll('.theme');
+    const timestamps = document.querySelectorAll('.timestamp');
+    const controllers = document.querySelectorAll('.controller');
 
     themes.forEach(theme => {
         theme.addEventListener('click', (e) => {
             changeTheme(e, display);
         });
     });
-}
 
-function setEvents() {
-    const display = document.querySelector('#timer');
-    const play = document.querySelector('#play-pause');
+    timestamps.forEach(timestamp => {
+        timestamp.addEventListener('click', (e) => {
+            setTimer(e, display, play);
+        });
+    });
 
-    timestampEvents(display);
-    controllerEvents(display);
-    themeEvents(display);
+    controllers.forEach(controller => {
+        controller.addEventListener('click', (e) => {
+            configCountdown(e, display, play);
+        });
+    });
 }
 
 setEvents();
